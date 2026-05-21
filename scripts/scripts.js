a14a45d92d520263369f455366d253da8caeb64b
mritunjayyadaveds <mritunjay.yadav@ibm.com>
2026-05-21 12:19:06 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -145,4 +145,10 @@ async function loadPage() {
   loadDelayed();
 }
 
+(async function loadDa() {
+  if (!new URL(window.location.href).searchParams.get('dapreview')) return;
+  import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
+}());
+
+
 loadPage();
